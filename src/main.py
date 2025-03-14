2844eaf3936cafa431f33bb31d6e2e4c0824c82d
zlaubacher <zlaubacher@gmail.com>
2025-03-14 02:04:41 -0400

    A: basic setup, establishment of main.py and some classes within textnode.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextType, TextNode
+
+def main():
+    text_node = TextNode("Don't Believe Ondore's Lies", TextType.ITALIC, url="https://youtu.be/abyKE9sZ6qc?si=pSvP52858y9-TmXS")
+    print(text_node)
+
+main()
